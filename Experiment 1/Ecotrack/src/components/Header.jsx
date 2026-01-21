6b2d2379da4311c464b28c1461a5e0876c5c8132
Bkg <135453953+chezyburgy@users.noreply.github.com>
2026-01-21 12:17:36 +0530

    Delete Experiment 1/Ecotrack directory

--- Experiment 1/Ecotrack/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = ({title}) =>{
-    return(
-        <header style = {{padding: "1 rem", background: "#2ca4ef",color: "white"}}>
-        <h>{title}</h>
-        </header>
-    );
-};
-
-export default Header;
